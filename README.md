9e13d3456f7839f8d7594fb968d5851a08f64c9b
Enzo Garcia <enzogar070503@gmail.com>
2024-06-30 18:45:34 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# FlaskAppMySql
+para que la aplicación funcione necesita una base de datos llamada "flaskapp" y ahí se importa la tabla que se encuentra en la carpeta table
---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from bountybotuniqueattestations import query_graphql
+
+app = Flask(__name__)
+
+# CONFIGURACION
+app.secret_key = 'mysecretkey'
+
+@app.route('/')
+def Index():
+    data = query_graphql()
+    
+    return render_template('index.html', wallets = data)
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
